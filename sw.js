bf4c31378ac39462579ea37eb4d8c927bcde348a
DK90 <kissdani90@gmail.com>
2025-12-13 19:43:51 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 sw.js
-const CACHE_NAME = 'fb-quiz-67'; /* VERZIÓ FRISSÍTVE (PWA telepíthetőség: index.html manifest link + app.js install init korábban) */
+const CACHE_NAME = 'fb-quiz-68'; /* VERZIÓ FRISSÍTVE (PWA telepíthetőség: index.html manifest link + app.js install init korábban) */
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
